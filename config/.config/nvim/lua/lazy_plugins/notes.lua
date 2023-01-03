7da8caacd4a2858d91b3ed4af4675f893eb2a518
4542elgh <4542elgh@gmail.com>
2023-01-03 11:03:50 -0800

    workpc wip

--- config/.config/nvim/lua/lazy_plugins/notes.lua
+++ config/.config/nvim/lua/lazy_plugins/notes.lua
@@ -5,8 +5,12 @@ return {
     {
         "vimwiki/vimwiki",
         config = function()
-            vim.cmd("let g:vimwiki_list = [{'path': stdpath('config') . '\\vimwiki', 'syntax': 'markdown', 'ext': '.md'}]")
-            abbrev("wiki", "VimwikiIndex")
+            -- This is not working on lazy.nvim for some reason
+            -- vim.cmd("let g:vimwiki_list = [{'path': stdpath('config') . '\\vimwiki', 'syntax': 'markdown', 'ext': '.md'}]")
+            --
+            -- Will use direct access for now
+            -- abbrev("wiki", "VimwikiIndex")
+            abbrev("wiki", "e " .. vim.fn.stdpath('config') .. '\\vimwiki\\index.md')
         end
     }
 }
--- config/.config/nvim/lua/lazy_plugins/telescope.lua
+++ config/.config/nvim/lua/lazy_plugins/telescope.lua
@@ -16,37 +16,16 @@ return {
 
     "4542elgh/telescope-smb-unc.nvim",
     "4542elgh/telescope-scratch-run.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-        cond = not vim.g.is_workpc
-    },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        config = function ()
-            require("telescope").setup({
-                extensions = {
-                    file_browser = {
-                        -- disables netrw and use telescope-file-browser in its place
-                        hijack_netrw = true,
-                        mappings = {
-                            ["i"] = {
-                              -- your custom insert mode mappings
-                            },
-                            ["n"] = {
-                              -- your custom normal mode mappings
-                            },
-                        },
-                    },
-                },
-            })
-        end,
-        -- cond = not vim.g.is_workpc
+        cond = not vim.g.is_workpc,
     },
     {
         "nvim-telescope/telescope.nvim",
         version = "0.1.0",
-        dependencies = {{"nvim-lua/plenary.nvim"}},
+        dependencies = {"nvim-lua/plenary.nvim"},
         config = function()
             function vimwiki()
                 require("telescope.builtin").find_files({
@@ -77,12 +56,37 @@ return {
                         -- }
                     }
                 },
+                extensions = {
+                    file_browser = {
+                        -- disables netrw and use telescope-file-browser in its place
+                        hijack_netrw = true,
+                        mappings = {
+                            ["i"] = {
+                                -- your custom insert mode mappings
+                            },
+                            ["n"] = {
+                              -- your custom normal mode mappings
+                            },
+                        },
+                    },
+                    scratch_run = {
+                        custom_compilers = {
+                            lua = {
+                                name = "lua",
+                                cmd = ":w !jit"
+                            }
+                        }
+                    }
+                },
             })
-            require("telescope").load_extension("fzf")
+            if not vim.g.is_workpc then
+                require("telescope").load_extension("fzf")
+            end
             require("telescope").load_extension("file_browser")
             require('telescope').load_extension('smb_unc')
             require('telescope').load_extension('scratch_run')
             -- require("telescope").load_extension("frecency")
+
         end,
         -- cond = not vim.g.is_workpc
     }
